ad0d7b48993310887f2a20ca72e80cc2e3cbe0d9
Dominik Huber <dhuber666@gmail.com>
2017-11-01 11:25:59 +0100

    Added ability to change the interval in the browser instead of in the code file itself

--- index.js
+++ index.js
@@ -1,68 +1,74 @@
 $(document).ready(function() {
-	
 	// global variables
-	const LEAGUE_URL = "http://api.pathofexile.com/ladders/harbinger";
+	const LEAGUE_URL = 'http://api.pathofexile.com/ladders/harbinger';
 	var buttonClicked = false;
 	var id;
+	var numberInput = $('#numberInput');
+	var todoMessage = $('#todoMessage');
+	var failDiv = $('#fail');
+	var successDiv = $('#success');
 
-	// fetches number of players you give it (maximum 200)
+	//fetches number of players you give it (maximum 200)
 	// returns: Array with the players
 	function fetchLimit(limit) {
-		
 		let link = LEAGUE_URL + `?limit=${limit}`;
 		console.log(link);
 
 		// API Call
 		$.ajax({
-		method: "GET",
-		url: link,
-		}).done(function(res, textStatus, xhr) {
-
-			console.log(xhr.status);
-			console.log("Response: ", res);
-			console.log("textStatus: ", textStatus);
-
-		}).fail(function(xhr) {
-			console.log(xhr.status);
-			endInterval();
-			buttonClicked = true;
+			method: 'GET',
+			url: link
 		})
+			.done(function(res, textStatus, xhr) {
+				console.log(xhr.status);
+				console.log('Response: ', res);
+				console.log('textStatus: ', textStatus);
+				successDiv.css('display', 'block');
+				failDiv.css('display', 'none');
+			})
+			.fail(function(xhr) {
+				failDiv.css('display', 'block');
+				successDiv.css('display', 'none');
+				console.log(xhr.status);
+				endInterval();
+				buttonClicked = true;
+			});
 	}
 
 	// click event listener for button
 	var button = document.getElementById('btn');
-	button.addEventListener("click", function() {
-		buttonClicked ? endInterval() : startInterval();
-		buttonClicked = !buttonClicked;
-		
-	})
+	button.addEventListener('click', function() {
+		if (numberInput.val()) {
+			buttonClicked ? endInterval() : startInterval(Number(numberInput.val()));
+			buttonClicked = !buttonClicked;
+		} else {
+			console.log(todoMessage);
+			todoMessage.html('No number - give me a number');
+		}
+	});
 	// clears timer and starts a 30 cooldown counter (then you can try the api call again)
 	function endInterval() {
 		clearInterval(id);
+		$('#input').css('display', 'none');
 		let timer = $('#timer');
 		console.log(timer);
 		let counter = 0;
 		id = setInterval(function() {
-			if(counter < 30) {
+			if (counter < 30) {
 				counter++;
 				timer.text(counter);
 			} else {
 				clearInterval(id);
 				buttonClicked = false;
-				startInterval();
+				$('#input').css('display', 'block');
 			}
-		}, 1000)
+		}, 1000);
 	}
-	// here you can try out the timer. Decrease it until you see it fail. 
-	function startInterval() {
-		
+	// here you can try out the timer. Decrease it until you see it fail.
+	function startInterval(interval) {
+		console.log(typeof interval);
 		id = setInterval(function() {
-			fetchLimit(5);
-		}, 10000)
+			fetchLimit(10);
+		}, interval);
 	}
-
-	
-
-
-
-})
+});
